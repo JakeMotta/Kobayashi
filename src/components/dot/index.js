c18ac3f1d0fef93b40b841ef8d09b6d5c0d89e06
Jake Motta <motta_jake@yahoo.com>
2022-05-12 13:19:15 -0700

    animation update

--- src/components/dot/index.js
+++ src/components/dot/index.js
@@ -5,6 +5,8 @@ const Dot = ({value, offset, animate}) => {
     const dotRef = useRef(null);
     const [transformA, setTransformA] = useState(false);
     const [transformB, setTransformB] = useState(false);
+    const [transformC, setTransformC] = useState(false);
+    const [transformD, setTransformD] = useState(false);
 
     useEffect(() => {
         if(animate) {
@@ -18,24 +20,45 @@ const Dot = ({value, offset, animate}) => {
     useEffect(() => {
         if(transformA) {
             dotRef.current.classList.add("transition");
-            dotRef.current.classList.add("dotCollapse");
-
+            dotRef.current.classList.add("blur");
+            dotRef.current.classList.add("dotTransition");
             let timer = setTimeout(() => {
                 clearTimeout(timer);
-                dotRef.current.classList.remove("transition");
-                dotRef.current.style.backgroundColor = "transparent";
                 setTransformB(true);
-            }, 1000)
+            }, 250)
         }
     }, [transformA])
 
     useEffect(() => {
         if(transformB) {
-            dotRef.current.children[0].style.color = "#2a2a2a";
-            dotRef.current.classList.add("textExpand");
-            dotRef.current.classList.add("transition");
+            dotRef.current.classList.remove("transition");
+            dotRef.current.classList.remove("blur");
+            dotRef.current.style.backgroundColor = "transparent";
+            setTransformC(true);
         }
     }, [transformB])
+
+    useEffect(() => {
+        if(transformC) {
+            dotRef.current.children[0].style.color = "#2a2a2a";
+            dotRef.current.classList.add("transition");
+            dotRef.current.classList.add("textInit");
+            dotRef.current.classList.add("blur");
+            let timer = setTimeout(() => {
+                clearTimeout(timer);
+                setTransformD(true);
+            }, 150)
+        }
+    }, [transformC])
+
+    useEffect(() => {
+        if(transformD) {
+            dotRef.current.classList.remove("textInit");
+            dotRef.current.classList.remove("blur");
+            dotRef.current.classList.add("textExpand");
+        }
+    }, [transformD])
+
 
     return (
         <div ref={dotRef} className={"dot"}>
--- src/components/invisible-text-input/index.js
+++ src/components/invisible-text-input/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./index.scss";
 
 const InvisibleTextInput = ({inputText, setInputText}) => (
-    <input id="kobayashi-input" value={inputText} onChange={(e) => setInputText(e.target.value)} autoFocus spellcheck="false" />
+    <input id="kobayashi-input" value={inputText} onChange={(e) => setInputText(e.target.value)} autoFocus spellCheck="false" />
 )
 
 export default InvisibleTextInput
